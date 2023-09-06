65e051d3a6ebae35dd0ab08495e76b8f76c9f7a9
ahab64 <merlin.burbach@gmx.de>
2023-09-06 15:50:19 +0200

    Klassen Tabelle und Klasse.User tabelle erstellt

--- backend/api/src/routes/apiRoutes.js
+++ backend/api/src/routes/apiRoutes.js
@@ -4,5 +4,13 @@ const { handleGetUser, handleLogin } = require('../controllers/loginController')
 
 router.get('/api/id/:email', handleGetUser);
 router.post('/api/login', handleLogin);
+router.post('/api/new-user', handleNewUser);
+
+//To Do: Endpunkte für...
+//EMails einer Klasse
+//Email eines Kurslehrers
+//Kurse pro User
+//Eintragung eins Klausurtermin (post)
+
 
 module.exports = router;
