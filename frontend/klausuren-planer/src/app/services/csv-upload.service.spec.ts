d30d7069962ce1d209ebb2686d81116938cfea4d
Rana Sajiel Ahmad <sajiel@MacBook-Pro-von-Rana.local>
2023-09-11 12:46:31 +0200

    added csv handling in angular

--- frontend/klausuren-planer/src/app/services/csv-upload.service.spec.ts
+++ frontend/klausuren-planer/src/app/services/csv-upload.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
+import { CsvParserService } from './csv-upload.service';
 
-import { CsvUploadService } from './csv-upload.service';
-
-describe('CsvUploadService', () => {
-  let service: CsvUploadService;
+describe('CsvParserService', () => {
+  let service: CsvParserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CsvUploadService);
+    service = TestBed.inject(CsvParserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should parse a CSV file', (done: DoneFn) => {
+    // Create a mock File object with sample CSV data
+    const csvData = 'Name,Email,Role\nJohn,Doe,Student\nJane,Smith,Teacher\n';
+    const blob = new Blob([csvData], { type: 'text/csv' });
+    const file = new File([blob], 'test.csv');
+
+    service.parseCSV(file).then((data) => {
+      // Assert that the data has been parsed correctly
+      expect(data).toEqual([
+        ['Name', 'Email', 'Role'],
+        ['John', 'Doe', 'Student'],
+        ['Jane', 'Smith', 'Teacher'],
+      ]);
+      done();
+    });
+  });
+
+  it('should handle errors when reading an invalid CSV file', (done: DoneFn) => {
+    // Create a mock invalid File object
+    const invalidBlob = new Blob(['Invalid,CSV,Data\nMissing Quotes'], { type: 'text/csv' });
+    const invalidFile = new File([invalidBlob], 'invalid.csv');
+
+    service.parseCSV(invalidFile).catch((error) => {
+      // Assert that an error message is received
+      expect(error).toBe('Error reading CSV file.');
+      done();
+    });
+  });
 });
+function beforeEach(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+
+function expect(service: CsvParserService) {
+  throw new Error('Function not implemented.');
+}
+
--- frontend/klausuren-planer/src/app/services/csv-upload.service.ts
+++ frontend/klausuren-planer/src/app/services/csv-upload.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CsvUploadService {
+export class CsvParserService {
+  parseCSV(file: File): Promise<string[][]> {
+    return new Promise<string[][]>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const csvText = e.target?.result as string;
+        const rows = csvText.split('\n');
+        const data: string[][] = [];
 
-  constructor() { }
+        for (const row of rows) {
+          const values = row.split(',');
+          data.push(values);
+        }
+
+        resolve(data);
+      };
+
+      reader.onerror = (e) => {
+        reject('Error reading CSV file.');
+      };
+
+      reader.readAsText(file);
+    });
+  }
 }
