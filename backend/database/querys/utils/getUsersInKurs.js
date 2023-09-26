d0a6975b6044d8dde3cfda5848e470b0df4c2726
Manken42 <fkildan9292@gmail.com>
2023-09-26 12:36:45 +0200

    added rolle to getEmailsFromKurse

--- backend/database/querys/utils/getUsersInKurs.js
+++ backend/database/querys/utils/getUsersInKurs.js
@@ -1,23 +1,23 @@
 async function getUsersInKurs(kurse, db) {
-    const sqlSelectEmails = `
-      SELECT User.email 
-      FROM User 
-      INNER JOIN "Kurs.User" ON User.user_id = "Kurs.User".user_id 
-      INNER JOIN Kurse ON "Kurs.User".kurs_id = Kurse.kurs_id 
-      WHERE Kurse.name = ?`;
-    
-    return new Promise((resolve, reject) => {
-      db.all(sqlSelectEmails, [kurse], function (err, rows) {
-        if (err) {
-          reject(err);
-        } else {
-          const emails = rows.map(row => row.email);
-          console.log('E-Mails der Benutzer im Kurs ' + kurse + ': ' + emails.join(', '));
-          resolve(emails);
-        }
-      });
+  const sqlSelectEmails = `
+    SELECT User.email, User.rolle
+    FROM User 
+    INNER JOIN "Kurs.User" ON User.user_id = "Kurs.User".user_id 
+    INNER JOIN Kurse ON "Kurs.User".kurs_id = Kurse.kurs_id 
+    WHERE Kurse.name = ?`;
+
+  return new Promise((resolve, reject) => {
+    db.all(sqlSelectEmails, [kurse], ['email', 'rolle'], function (err, rows) {
+      if (err) {
+        reject(err);
+      } else {
+        const emails = rows.map(row => `${row.email}`);
+        const rolle = rows.map(row => `${row.rolle}`);
+        const response = { emails, rolle };
+        resolve(response);
+      }
     });
-  }
-  
-  module.exports = { getUsersInKurs };
-  
+  });
+}
+
+module.exports = { getUsersInKurs };
