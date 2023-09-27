7c23943668784102a927138822cf326f24d19b95
Rana Sajiel Ahmad <sajiel@MacBook-Pro-von-Rana.local>
2023-09-27 10:23:28 +0200

    ka

--- frontend/klausuren-planer/src/app/csv-upload/csv-upload.component.ts
+++ frontend/klausuren-planer/src/app/csv-upload/csv-upload.component.ts
@@ -1,57 +1,56 @@
+import { CsvUploadService } from '../services/csv-upload.service';
+
 import { Component } from '@angular/core';
+import * as Papa from 'papaparse';
 
 @Component({
   selector: 'app-csv-upload',
   templateUrl: './csv-upload.component.html',
   styleUrls: ['./csv-upload.component.css']
 })
-
-
 export class CsvUploadComponent {
   selectedFile: File | null = null;
-  csvData: any[] = []; // Array to store CSV data
 
-  constructor() { }
+  constructor(private csvUploadService: CsvUploadService) { }
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
-    
-    // Read and parse the selected CSV file
+  }
+
+  parseAndSendCsv() {
     if (this.selectedFile) {
       const fileReader = new FileReader();
 
       fileReader.onload = (e: any) => {
-        const csvData = e.target.result;
-        this.csvData = this.parseCsvData(csvData);
-        console.log(csvData);
+        // Parse the CSV data using PapaParse
+        Papa.parse(e.target.result, {
+          header: true, // Treat the first row as headers
+          dynamicTyping: true, // Automatically detect data types
+          skipEmptyLines: true, // Skip empty lines
+          complete: (result: any) => {
+            // Convert the result to JSON
+            const jsonData = result.data;
+            console.log(jsonData);
+
+            // Send the JSON data to the CsvUploadService's sendCsvToApi method
+            this.csvUploadService.sendJsonToApi(jsonData).subscribe(
+              (response: any) => {
+                console.log('API Response:', response);
+              },
+              (error: any) => {
+                console.error('API Error:', error);
+              }
+            );
+          },
+          error: (error: any) => {
+            console.error('CSV parsing error:', error.message);
+          }
+        });
       };
 
       fileReader.readAsText(this.selectedFile);
+    } else {
+      console.error('No CSV file selected.');
     }
-  }
-
-  private parseCsvData(csvData: string): any[] {
-    // Implement CSV parsing logic here to convert the CSV data to an array of objects.
-    // For simplicity, we'll assume a basic CSV format (comma-separated values).
-    const lines = csvData.split('\n');
-    const headers = lines[0].split(',');
-
-    const data = [];
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
-    
-      // Check if the row is empty or has fewer values than headers
-      if (values.length === headers.length) {
-        const row: Record<string, any> = {};
-    
-        for (let j = 0; j < headers.length; j++) {
-          row[headers[j]] = values[j];
-        }
-    
-        data.push(row);
-      }
-    }
-
-    return data;
   }
 }
